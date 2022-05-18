a640302f2d4659957e20c9053550fa79a89d8ded
kjw-blog <rkdwjddnr11@naver.com>
2022-05-18 16:41:16 +0900

    전체삭제 기능 추가

--- components/board/Post.jsx
+++ components/board/Post.jsx
@@ -1,7 +1,56 @@
-export default function Post({ text }) {
+export default function Post({
+  text,
+  list,
+  setList,
+  deleteLoading,
+  setDeleteLoading,
+}) {
+  const deletePost = (e) => {
+    if (deleteLoading) {
+      alert('다른 포스트잇을 삭제중입니다 잠시 후 다시 삭제해주세요.');
+      return;
+    }
+
+    setDeleteLoading(true);
+    const style = e.target.style;
+    e.target.animate(
+      [
+        {
+          transform: 'translateX(0px)',
+        },
+        {
+          transform: 'translateX(-500px)',
+        },
+      ],
+      {
+        duration: 600,
+        iterations: 1,
+      }
+    );
+    const newList = list.filter((post) => post !== text);
+    localStorage.clear();
+
+    localStorage.setItem('list', JSON.stringify(newList));
+
+    setTimeout(() => {
+      style.height = '0px';
+      style.margin = '0px';
+
+      setTimeout(() => {
+        setList(newList);
+        setDeleteLoading(false);
+      }, 200);
+    }, 400);
+  };
+
   return (
-    <li className="border-l-[25px] border-l-yellow-300 flex items-center bg-yellow-200 h-12 px-4 text-gray-500 font-bold text-sm transition-all focus:shadow-hover focus:rounded-br-hover shadow-post hover:shadow-hover rounded-br-post hover:rounded-br-hover">
-      {text}
-    </li>
+    <>
+      <li
+        onClick={deletePost}
+        className="border-l-[25px] z-50 border-l-yellow-300 flex items-center overflow-hidden bg-yellow-200 h-12  px-4 text-gray-500 font-bold text-sm transition-all focus:shadow-hover focus:rounded-br-hover shadow-post hover:shadow-hover rounded-br-post hover:rounded-br-hover"
+      >
+        {text.slice(0, text.lastIndexOf('_'))}
+      </li>
+    </>
   );
 }
--- components/common/Board.jsx
+++ components/common/Board.jsx
@@ -2,10 +2,10 @@ import { useState, useEffect } from 'react';
 import Image from 'next/image';
 
 import Post from '../board/Post';
-import { list } from 'postcss';
 
 export default function Board() {
   const [render, setRender] = useState(false);
+  const [deleteLoading, setDeleteLoading] = useState(false);
   const [postList, setPostList] = useState([]);
 
   const insert = (e) => {
@@ -15,7 +15,10 @@ export default function Board() {
       if (text) {
         localStorage.clear();
 
-        localStorage.setItem('list', JSON.stringify([...postList, text]));
+        localStorage.setItem(
+          'list',
+          JSON.stringify([...postList, text + '_' + +new Date()])
+        );
         e.target.value = '';
         setRender((prev) => !prev);
       } else {
@@ -23,11 +26,19 @@ export default function Board() {
       }
     }
   };
+  const allDelete = () => {
+    if (confirm('전체 포스트를 삭제할까요?')) {
+      localStorage.clear();
+      setRender((prev) => !prev);
+    }
+  };
 
   useEffect(() => {
     const list = localStorage.getItem('list');
     if (list) {
       setPostList(JSON.parse(list));
+    } else {
+      setPostList([]);
     }
   }, [render]);
 
@@ -37,11 +48,36 @@ export default function Board() {
         <div className="overflow-y-scroll scrollbar-hide h-full w-full">
           <ul className="grid transition-all grid-cols-1 justify-start space-y-5 items-start">
             {postList.length > 0 &&
-              postList.map((post) => <Post key={post} text={post} />)}
+              postList.map((post) => (
+                <Post
+                  key={post}
+                  text={post}
+                  list={postList}
+                  setList={setPostList}
+                  deleteLoading={deleteLoading}
+                  setDeleteLoading={setDeleteLoading}
+                />
+              ))}
           </ul>
         </div>
-        <div className="absolute top-0 right-0">
+        <div className="absolute top-5 right-10">
           <Image src="/pin.png" alt="pin" width={64} height={49.6} />
+        </div>
+        <div className="absolute top-7 left-10" onClick={allDelete}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-8 w-8 text-gray-800 cursor-pointer"
+            fill="#EAEAEA"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth="2"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+            />
+          </svg>
         </div>
       </div>
       <input
